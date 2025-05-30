a8f3927824f2bd6f061585c40da1fcc4d2a0c0dc
kaue-cunha <kaueian.study@gmail.com>
2025-05-30 18:18:07 -0300

    feat: entrega final do projeto

--- core/models/editora.py
+++ core/models/editora.py
@@ -3,8 +3,8 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
-    email = models.EmailField(blank=True, null=True)  # Campo adicionado
-    cidade = models.CharField(max_length=255, blank=True, null=True)  # Opcional, caso queira cidade
+    email = models.EmailField(max_length=100, blank=True, null=True)
+    cidade = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.nome
