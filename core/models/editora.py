52068c15bcc6278217861b2b42fe1ad57cc23f16
kaue-cunha <kaueian.study@gmail.com>
2025-05-30 17:55:01 -0300

    fix: correção de erros no arquivo editora.py na pasta models.

--- core/models/editora.py
+++ core/models/editora.py
@@ -3,6 +3,8 @@ from django.db import models
 class Editora(models.Model):
     nome = models.CharField(max_length=100)
     site = models.URLField(max_length=200, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)  # Campo adicionado
+    cidade = models.CharField(max_length=255, blank=True, null=True)  # Opcional, caso queira cidade
 
     def __str__(self):
         return self.nome
